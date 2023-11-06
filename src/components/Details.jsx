7972c5211efd44c3aca7f2bca54222d652fafe6d
Abai <abaiesen04@gmail.com>
2023-11-06 14:25:25 +0600

    add details

---
+++ src/components/Details.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const styles = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex'
+};
+
+export default function Details({image, title, description, open, handleClose}) {
+  
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={styles}>
+            <div><img width={300} src={image} alt="" /></div>
+            <div><h1>{title}</h1>
+            description: {description}</div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
