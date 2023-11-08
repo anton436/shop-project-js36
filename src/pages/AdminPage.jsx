3b7991130d87abda37599cd34477391b3f8964a2
Anton <anton@MacBook-Pro-Anton.local>
2023-11-08 12:06:40 +0600

    .

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,12 +1,20 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import AddCategoryModal from '../components/product/AddCategoryModal';
 import AddProduct from '../components/product/AddProduct';
 
 const AdminPage = () => {
-    return (
-        <div>
-            <AddProduct/>
-        </div>
-    );
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <Button onClick={handleOpen}>Add Category</Button>
+
+      <AddProduct />
+      <AddCategoryModal open={open} handleClose={handleClose} />
+    </div>
+  );
 };
 
 export default AdminPage;
